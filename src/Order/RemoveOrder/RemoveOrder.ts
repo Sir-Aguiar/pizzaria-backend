32ae59a7ba3c8cc43107f16b8262de54265e9166
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-03-27 13:16:52 -0400

    Remove Order

---
+++ src/Order/RemoveOrder/RemoveOrder.ts
@@ -0,0 +1,9 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { OrdersDB } from "../../db/FirebaseInitialize";
+
+export class RemoveOrder {
+  constructor(readonly orderId: string) {}
+  public async execute(test = false) {
+    await deleteDoc(doc(OrdersDB, `${test ? "Pedidos_Teste" : "Pedidos"}`, this.orderId));
+  }
+}
