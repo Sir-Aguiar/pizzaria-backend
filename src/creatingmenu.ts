692cf73d1990537424b626dd6a51f6ac8ce427bf
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-04-23 16:17:40 -0400

    Lil change

--- src/creatingmenu.ts
+++ src/creatingmenu.ts
@@ -2,10 +2,11 @@ import * as menu from "../menu.json";
 import { updateDoc, doc, arrayUnion } from "firebase/firestore";
 import { OrdersDB } from "./Firebase/FirebaseInitialize";
 import { UniqScript } from "./generateUniqCodeScript";
+
 const insertToData = async () => {
   menu.Lanches.forEach((lanche) => {
     updateDoc(doc(OrdersDB, "Menus", "TestePizzariaMenu"), {
-      Produtos: arrayUnion({
+      Lanches: arrayUnion({
         name: lanche.name,
         description: lanche.description,
         price: lanche.price,
@@ -15,6 +16,7 @@ const insertToData = async () => {
     });
   });
 };
+
 insertToData().then((res) => {
   console.log("Jóia");
 });
